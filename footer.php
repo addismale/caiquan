45849381458fdd8c73377805256fb88c0b5e0de6
maxiaotiao <addismale@gmail.com>
2016-03-29 12:49:15 +0800

    财圈项目logo与导航条

--- footer.php
+++ footer.php
@@ -1,8 +1,7 @@
-   <body>
-   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
 
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="/js/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,8 +1,35 @@
 <?php include "header.php" ?>
-<body>
-  <div class="shangmian">
-    <h1 class="caiquan" >  </h1>
-  </div> 
-</body>
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+           <!-- 小屏幕按钮  和loogo-->
+        <div class="navbar-header">
+          <button class="navbar-toggle"data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span> 
+              <span class="icon-bar"></span> 
+          </button>
+          <a href="#" class="navbar-brand">
+            <img class="logo" src="/image/caiquan.png">
+          </a>
+        </div>
+           <!-- 小屏幕按钮  和loogo-->
+           <!--导航-->
+        <div class="collapse navbar-collapse">
+           <ul class="nav navbar-nav navbar">
+               <li><a href="#">首页</a></li>
+               <li><a href="#">活动</a></li>
+               <li><a href="#">课程</a></li>
+               <li><a href="#">生活</a></li>
+               <li><a href="#">关于我们</a></li>
+           </ul>
+           <div class="fr user-enter">
+               <img src="/image/denglu.png">
+               <a class="gjs-devise-link" href="#">登录</a>
+               <a class="gjs-devise-link" href="#">登录</a>
+          </div>
+        </div>
+          <!--导航-->         
+      </div>
+ </nav> 
 <?php include "footer.php" ?>
 
