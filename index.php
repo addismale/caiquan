92ec541d49990eb8682078c1807dd3ff06b0d8fc
maxiaotiao <addismale@gmail.com>
2016-03-30 12:48:42 +0800

    轮播插件

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include "header.php" ?>
- <nav class="navbar navbar-inverse navbar-fixed-top">
+<nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
            <!-- 小屏幕按钮  和loogo-->
         <div class="navbar-header">
@@ -28,8 +28,26 @@
                <a class="gjs-devise-link" href="#">登录</a>
           </div>
         </div>
-          <!--导航-->         
+          <!--导航-->
+          <!-- -->
+         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+           <ol class="carousel-indicators">
+             <li data-target="#carousel-example-generic" data-slide-to="0" class=""></li>
+             <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+             <li data-target="#carousel-example-generic" data-slide-to="2" class="active"></li>
+          </ol>
+        <div class="carousel-inner" role="listbox">
+          <div class="item">
+            <img src="/image/datu.png">
+          </div>
+          <div class="item">
+            <img src="/image/datu.png" >
+          </div>
+          <div class="item active">
+            <img src="/image/datu.png" >       
+          </div>
       </div>
- </nav> 
-<?php include "footer.php" ?>
+  </div>
+</nav> 
+ <?php include "footer.php" ?>
 
