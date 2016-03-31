4031262f1d56e6c670fed5633a7e6a7d7773acd6
maxiaotiao <addismale@gmail.com>
2016-03-31 10:52:55 +0800

    生活 活动 课程目录

--- index.php
+++ index.php
@@ -51,21 +51,20 @@
 </nav> 
 <!-- 生活部分 -->
 <div class="container">
-<div class="shenghuo">
-  <div class="shenghuo1"> 
+    <div class="shenghuo">
+      <div class="shenghuo1"> 
     <div class="cn">生活</div>
     <div class="en">Financial Life</div>
-  </div>
+    </div>
 <div class="More">
 <a class="More-a">More</a>
 <a class="More-tu"><a>
 </div>
 </div>
 <!--生活目录 -->
-   <div class="container">
-     <div class="col-md-4">
+<div class="col-md-4">
         <div class="li1">
-            <a href="" target="blank">
+            <a href="#">
                 <img class="life-image" src="/image/daijin.jpg" >
             </a>
         </div>
@@ -94,10 +93,185 @@
         <div class="desc">
           <div class="name"><a href="" target="blank">宝马限时优惠卷</a></div>
              <div class="time">2016-02-04 15:50:06</div>
-          </div>
+        </div>
 
      </div> 
+
+
    </div>
+</div>
+<!-- 主页活动页面-->
+
+<div class="container">
+    <div class="huodong">
+       <div class="huodong1">
+            <div class="hd">活动</div>
+            <div class="ay">Activity</div>
+       </div>
+       <div class="More">
+           <a class="More-a">More</a>
+           <a class="More-tu"></a>
+       </div>
+    </div>
+
+    <div class="col-md-12">
+    <div class="col-md-4">
+       <a href="#">
+           <img src="/image/wenzi1.png">
+       </a>
+    </div>
+    <div class="col-md-4">
+      <dl class="wenben1">
+           <dt><a href="/events/915"> 国人必修课——理财知识论坛</a></dt>
+           <dd><span>主办方：</span>国金集团</dd>
+           <dd><span>活动时间：</span>2016-03-31 00:00:00 ~ 2016-03-31 00:00:00</dd>
+           <dd><span>活动地点：</span>  广东深圳罗湖区京基100大厦A座</dd>
+           <dd><span>报名费用：</span>免费</dd>
+           <dd><span>发布时间：</span>2016-03-30 11:43:11</dd>
+      </dl>
+    </div>
+    <div class="col-md-4">
+       <div class="links">
+           <a href="#" class="btn btn1">查看详情</a>
+           <a href="#" class="btn btn2">我要报名</a>
+       </div>
+    </div>
+</div>
+ <div class="col-md-12">
+    <div class="col-md-4">
+       <a href="#">
+           <img src="/image/wenzi2.png">
+       </a>
+    </div>
+    <div class="col-md-4">
+      <dl class="wenben1">
+           <dt><a href="/events/915"> 国人必修课——理财知识论坛</a></dt>
+           <dd><span>主办方：</span>国金集团</dd>
+           <dd><span>活动时间：</span>2016-03-31 00:00:00 ~ 2016-03-31 00:00:00</dd>
+           <dd><span>活动地点：</span>  广东深圳罗湖区京基100大厦A座</dd>
+           <dd><span>报名费用：</span>免费</dd>
+           <dd><span>发布时间：</span>2016-03-30 11:43:11</dd>
+      </dl>
+    </div>
+    <div class="col-md-4">
+       <div class="links">
+           <a href="#" class="btn btn1">查看详情</a>
+           <a href="#" class="btn btn2">我要报名</a>
+       </div>
+    </div>
+</div>
+
+ <div class="col-md-12">
+    <div class="col-md-4">
+       <a href="#">
+           <img src="/image/wenzi3.png">
+       </a>
+    </div>
+    <div class="col-md-4">
+      <dl class="wenben1">
+           <dt><a href="/events/915"> 国人必修课——理财知识论坛</a></dt>
+           <dd><span>主办方：</span>国金集团</dd>
+           <dd><span>活动时间：</span>2016-03-31 00:00:00 ~ 2016-03-31 00:00:00</dd>
+           <dd><span>活动地点：</span>  广东深圳罗湖区京基100大厦A座</dd>
+           <dd><span>报名费用：</span>免费</dd>
+           <dd><span>发布时间：</span>2016-03-30 11:43:11</dd>
+      </dl>
+    </div>
+    <div class="col-md-4">
+       <div class="links">
+           <a href="#" class="btn btn1">查看详情</a>
+           <a href="#" class="btn btn2">我要报名</a>
+       </div>
+    </div>
+</div>
+</div>
+
+ 
+
+ <div class="container">
+    <div class="huodong">
+       <div class="huodong1">
+            <div class="hd">课程</div>
+            <div class="ay">Course</div>
+       </div>
+       <div class="More">
+           <a class="More-a">More</a>
+           <a class="More-tu"></a>
+       </div>
+    </div>
+
+    <div class="col-md-12">
+    <div class="col-md-4">
+       <a href="#">
+           <img src="/image/wenzi4.png">
+       </a>
+    </div>
+    <div class="col-md-4">
+      <dl class="wenben1">
+           <dt><a href="/events/915"> 国人必修课——理财知识论坛</a></dt>
+           <dd><span>主办方：</span>国金集团</dd>
+           <dd><span>活动时间：</span>2016-03-31 00:00:00 ~ 2016-03-31 00:00:00</dd>
+           <dd><span>活动地点：</span>  广东深圳罗湖区京基100大厦A座</dd>
+           <dd><span>报名费用：</span>免费</dd>
+           <dd><span>发布时间：</span>2016-03-30 11:43:11</dd>
+      </dl>
+    </div>
+    <div class="col-md-4">
+       <div class="links">
+           <a href="#" class="btn btn1">查看详情</a>
+           <a href="#" class="btn btn2">我要报名</a>
+       </div>
+    </div>
+</div>
+ <div class="col-md-12">
+    <div class="col-md-4">
+       <a href="#">
+           <img src="/image/wenzi5.png">
+       </a>
+    </div>
+    <div class="col-md-4">
+      <dl class="wenben1">
+           <dt><a href="/events/915"> 国人必修课——理财知识论坛</a></dt>
+           <dd><span>主办方：</span>国金集团</dd>
+           <dd><span>活动时间：</span>2016-03-31 00:00:00 ~ 2016-03-31 00:00:00</dd>
+           <dd><span>活动地点：</span>  广东深圳罗湖区京基100大厦A座</dd>
+           <dd><span>报名费用：</span>免费</dd>
+           <dd><span>发布时间：</span>2016-03-30 11:43:11</dd>
+      </dl>
+    </div>
+    <div class="col-md-4">
+       <div class="links">
+           <a href="#" class="btn btn1">查看详情</a>
+           <a href="#" class="btn btn2">我要报名</a>
+       </div>
+    </div>
+</div>
+
+ <div class="col-md-12">
+    <div class="col-md-4">
+       <a href="#">
+           <img src="/image/wenzi1.png">
+       </a>
+    </div>
+    <div class="col-md-4">
+      <dl class="wenben1">
+           <dt><a href="/events/915"> 国人必修课——理财知识论坛</a></dt>
+           <dd><span>主办方：</span>国金集团</dd>
+           <dd><span>活动时间：</span>2016-03-31 00:00:00 ~ 2016-03-31 00:00:00</dd>
+           <dd><span>活动地点：</span>  广东深圳罗湖区京基100大厦A座</dd>
+           <dd><span>报名费用：</span>免费</dd>
+           <dd><span>发布时间：</span>2016-03-30 11:43:11</dd>
+      </dl>
+    </div>
+    <div class="col-md-4">
+       <div class="links">
+           <a href="#" class="btn btn1">查看详情</a>
+           <a href="#" class="btn btn2">我要报名</a>
+       </div>
+    </div>
+</div>
+  
+
 </div>
 
 
