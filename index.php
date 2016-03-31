abf9d86b40f6b06369fbb6ea2018f44e2124d657
maxiaotiao <addismale@gmail.com>
2016-03-31 14:51:33 +0800

    项目结束

--- index.php
+++ index.php
@@ -270,10 +270,46 @@
        </div>
     </div>
 </div>
-  
-
 </div>
 
 
- <?php include "footer.php" ?>
+<div class="footer"> 
+  <div class="footera">
+      <div class="col-md-4">
+      <dl>
+        <dt>财圈</dt>
+          <dd>
+            <p>400-633-3689</p>
+            <a href="mailto:media@caiquan.com">Email: media@caiquan.com</a>
+            <p>
+            <a href="/">首页</a>
+            <a href="/courses">课程</a>
+            <a href="/events">活动</a>
+            <a href="/promotions">生活</a>
+            <a href="/about">关于我们</a>
+            </p>
+           </dd>
+         </dl> 
+       </div> 
+    </div> 
+<div class="col-md-4">
+   <dl>
+      <dt>关注微博</dt>
+        <dd class="corner">
+          <img src="/image/qrcode.jpg">
+        </dd>
+   </dl>  
+</div>
+
+<div class="col-md-4">
+   <dl>
+      <dt>关注微信</dt>
+        <dd class="corner">
+        <img src="/image/qrcode_for_cq.jpg">
+        </dd>
+   </dl>  
+</div>
+
+</div>
+<?php include "footer.php" ?>
 
