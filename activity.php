563ed88493e41360fb8bba4f7c1438f9791eea14
maxiaotiao <addismale@gmail.com>
2016-04-03 18:48:20 +0800

    活动页面课程页面生活页面

---
+++ activity.php
@@ -0,0 +1,136 @@
+<?php include "header.php" ?> 
+ 
+<div class="container">
+    <div class="activitya">
+      <a href="/events?c=new" class="selected">最新活动</a>
+      <a href="/events?c=Normal" class="">常规活动</a>
+      <a href="/events?c=CfoWisdom" class="">CFO Wisdom</a>
+      <a href="/events?c=TaxWisdom" class="">Tax Wisdom</a>
+      <a href="/events?c=TreasuryWisdom" class=""> 定制精选</a>
+      <a href="/events?c=Business" class="">论坛峰会</a>
+      <a href="/events?c=Forum" class="">海外速递</a>
+      <a href="/events?c=old" class="">往期活动</a>
+    </div>
+  </div> 
+<div class="container">
+   
+    <div class="col-md-12">
+    <div class="col-md-4">
+       <a href="#">
+           <img src="/image/wenzi1.png">
+       </a>
+    </div>
+    <div class="col-md-4">
+      <dl class="wenben1">
+           <dt>
+           <span class="fr tag">最新课程</span>
+           <a href="/events/915"> 国人必修课——理财知识论坛</a>
+           </dt>
+           <dd><span>主办方：</span>国金集团</dd>
+           <dd><span>活动时间：</span>2016-03-31 00:00:00 ~ 2016-03-31 00:00:00</dd>
+           <dd><span>活动地点：</span>  广东深圳罗湖区京基100大厦A座</dd>
+           <dd><span>报名费用：</span>免费</dd>
+           <dd><span>发布时间：</span>2016-03-30 11:43:11</dd>
+      </dl>
+    </div>
+    <div class="col-md-4">
+       <div class="links">
+           <a href="#" class="btn btn1">查看详情</a>
+           <a href="#" class="btn btn2">我要报名</a>
+       </div>
+    </div>
+</div>
+ <div class="col-md-12">
+    <div class="col-md-4">
+       <a href="#">
+           <img src="/image/wenzi2.png">
+       </a>
+    </div>
+    <div class="col-md-4">
+      <dl class="wenben1">
+           <dt><a href="/events/915"> 国人必修课——理财知识论坛</a></dt>
+           <dd><span>主办方：</span>国金集团</dd>
+           <dd><span>活动时间：</span>2016-03-31 00:00:00 ~ 2016-03-31 00:00:00</dd>
+           <dd><span>活动地点：</span>  广东深圳罗湖区京基100大厦A座</dd>
+           <dd><span>报名费用：</span>免费</dd>
+           <dd><span>发布时间：</span>2016-03-30 11:43:11</dd>
+      </dl>
+    </div>
+    <div class="col-md-4">
+       <div class="links">
+           <a href="#" class="btn btn1">查看详情</a>
+           <a href="#" class="btn btn2">我要报名</a>
+       </div>
+    </div>
+</div>
+
+ <div class="col-md-12">
+    <div class="col-md-4">
+       <a href="#">
+           <img src="/image/wenzi3.png">
+       </a>
+    </div>
+    <div class="col-md-4">
+      <dl class="wenben1">
+           <dt><a href="/events/915"> 国人必修课——理财知识论坛</a></dt>
+           <dd><span>主办方：</span>国金集团</dd>
+           <dd><span>活动时间：</span>2016-03-31 00:00:00 ~ 2016-03-31 00:00:00</dd>
+           <dd><span>活动地点：</span>  广东深圳罗湖区京基100大厦A座</dd>
+           <dd><span>报名费用：</span>免费</dd>
+           <dd><span>发布时间：</span>2016-03-30 11:43:11</dd>
+      </dl>
+    </div>
+    <div class="col-md-4">
+       <div class="links">
+           <a href="#" class="btn btn1">查看详情</a>
+           <a href="#" class="btn btn2">我要报名</a>
+       </div>
+    </div>
+
+</div>
+</div>
+
+
+<div class="container">
+       <div class="col-md-12">
+    <div class="col-md-4">
+       <a href="#">
+           <img src="/image/wenzi1.png">
+       </a>
+    </div>
+    <div class="col-md-4">
+      <dl class="wenben1">
+           <dt><a href="/events/915"> 国人必修课——理财知识论坛</a></dt>
+           <dd><span>主办方：</span>国金集团</dd>
+           <dd><span>活动时间：</span>2016-03-31 00:00:00 ~ 2016-03-31 00:00:00</dd>
+           <dd><span>活动地点：</span>  广东深圳罗湖区京基100大厦A座</dd>
+           <dd><span>报名费用：</span>免费</dd>
+           <dd><span>发布时间：</span>2016-03-30 11:43:11</dd>
+      </dl>
+    </div>
+    <div class="col-md-4">
+       <div class="links">
+           <a href="#" class="btn btn1">查看详情</a>
+           <a href="#" class="btn btn2">我要报名</a>
+       </div>
+    </div>
+</div>
+</div>
+
+<style>
+.activitya{
+  width: 1140px;
+  height: 80px; 
+  padding-top: 30px;
+}
+.activitya a{
+  margin-left: 65px;  
+  color: #6a6a6a;
+  font-size: 16px;  
+}
+.activitya a:hover{
+  color: #33a1ff; 
+}
+</style>
+ <?php include "footer.php" ?>
+
---
+++ lifr.php
@@ -0,0 +1,134 @@
+<?php include "header.php" ?>
+<div class="container">
+  <ul class="lifr_mulu">
+    <li>
+      <div class="mulu1">
+        <a href="#">
+          <img src="/image/lifrtu1.png" width=450">
+        </a>  
+      </div>
+      <div class="mulu2">
+        <div class="tuwenzi1"> 
+          <a> 宝马代金卷100元</a> 
+        </div>
+        <p>网厅代金券仅限减免单购手机或合约机手机差价部分，不能够减免号码预存、话费协议预存款、以 及配件金额，即不能优惠单号码、单套餐、配件、手机差价为0（零元购机档）的产品； 每个订单仅限使用一张，每张券仅限使用一次。</p>
+        <div class="lianjie">
+        <a href="#" class="btn btn2">购买</a>          
+        </div>
+      </div>
+    </li>
+    <li>
+       <div class="mulu1">
+        <a href="#">
+          <img src="/image/lifrtu2.png" width=450">
+        </a>  
+      </div>
+      <div class="mulu2">
+        <div class="tuwenzi1"> 
+          <a> 宝马代金卷100元</a> 
+        </div>
+        <p>网厅代金券仅限减免单购手机或合约机手机差价部分，不能够减免号码预存、话费协议预存款、以 及配件金额，即不能优惠单号码、单套餐、配件、手机差价为0（零元购机档）的产品； 每个订单仅限使用一张，每张券仅限使用一次。</p>
+        <div class="lianjie">
+          <a href="#" class="btn btn2">购买</a>    
+        </div>
+      </div>
+
+    </li>
+    <li>
+      <div class="mulu1">
+        <a href="#">
+          <img src="/image/lifrtu3.png" width=450">
+        </a>  
+      </div>
+      <div class="mulu2">
+        <div class="tuwenzi1"> 
+          <a> 宝马代金卷100元</a> 
+        </div>
+        <p>网厅代金券仅限减免单购手机或合约机手机差价部分，不能够减免号码预存、话费协议预存款、以 及配件金额，即不能优惠单号码、单套餐、配件、手机差价为0（零元购机档）的产品； 每个订单仅限使用一张，每张券仅限使用一次。</p>
+        <div class="lianjie">
+          <a href="#" class="btn btn2">购买</a>    
+        </div>
+      </div>
+    </li>
+    <li>
+     <div class="mulu1">
+        <a href="#">
+          <img src="/image/lifrtu4.png" width=450">
+        </a>  
+      </div>
+      <div class="mulu2">
+        <div class="tuwenzi1"> 
+          <a> 宝马代金卷100元</a> 
+        </div>
+        <p>网厅代金券仅限减免单购手机或合约机手机差价部分，不能够减免号码预存、话费协议预存款、以 及配件金额，即不能优惠单号码、单套餐、配件、手机差价为0（零元购机档）的产品； 每个订单仅限使用一张，每张券仅限使用一次。</p>
+        <div class="lianjie">
+          <a href="#" class="btn btn2">购买</a>    
+        </div>
+      </div>
+    </li>
+    <li>
+    <div class="mulu1">
+        <a href="#">
+          <img src="/image/lifrtu1.png" width=450">
+        </a>  
+      </div>
+      <div class="mulu2">
+        <div class="tuwenzi1"> 
+          <a> 宝马代金卷100元</a> 
+        </div>
+        <p>网厅代金券仅限减免单购手机或合约机手机差价部分，不能够减免号码预存、话费协议预存款、以 及配件金额，即不能优惠单号码、单套餐、配件、手机差价为0（零元购机档）的产品； 每个订单仅限使用一张，每张券仅限使用一次。</p>
+        <div class="lianjie">
+          <a href="#" class="btn btn2">购买</a>    
+        </div>
+      </div>
+    </li>
+    <li>
+    <div class="mulu1">
+        <a href="#">
+          <img src="/image/lifrtu2.png" width=450">
+        </a>  
+      </div>
+      <div class="mulu2">
+        <div class="tuwenzi1"> 
+          <a> 宝马代金卷100元</a> 
+        </div>
+        <p>网厅代金券仅限减免单购手机或合约机手机差价部分，不能够减免号码预存、话费协议预存款、以 及配件金额，即不能优惠单号码、单套餐、配件、手机差价为0（零元购机档）的产品； 每个订单仅限使用一张，每张券仅限使用一次。</p>
+        <div class="lianjie">
+          <a href="#" class="btn btn2">购买</a>    
+        </div>
+      </div>
+    
+    </li>
+  </ul>
+</div>
+
+<style>
+.lifr_mulu{
+  width: 1180px;
+}
+.mulu1{
+  width: 510px;
+  margin-top: 20px;
+}
+.mulu2 p{
+  width: 450px;
+  font-size: 13px;
+  margin-top: 10px;
+}
+.lifr_mulu li{
+ list-style-type:none;  
+ width: 500px;
+ float: left;
+}
+.tuwenzi1 a{
+  color: black;
+  font-size: 18px;
+}
+.tuwenzi1{
+  margin-top: 15px;
+}
+</style>
+
+<?php include "footer.php" ?>
+
+
